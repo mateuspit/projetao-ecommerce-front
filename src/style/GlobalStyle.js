a6ee158150e73bcdd04779f0fe4e9b8d889816d4
Mateus Barcelos <mateuspit@hotmail.com>
2023-04-26 15:16:37 -0300

    init project, librarys and files

---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+import { mainColorButton, mainContrastColor } from "../constants/colors";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+`
+
+export default GlobalStyle
