dc5b171d09e199c4ea87d20baf4fd6d058a7ca6d
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-04-26 16:35:22 -0300

    feat: set routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React from "react";
+import styled from "styled-components"
+
+import { UserContext } from "./context/UserContext"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import Product from "./pages/Product"
+
+export default function App() {
+
   return (
-    <>
-      <h1>test</h1>
-    </>
-  );
+    <PagesContainer>
+      <BrowserRouter>
+        <UserContext.Provider >
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<SignUp />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/:id" element={<Product />} />
+          </Routes>
+        </UserContext.Provider >
+      </BrowserRouter >
+    </PagesContainer >
+  )
 }
 
-export default App;
+const PagesContainer = styled.main`
+  width: 100vw;
+  height: 100vw;
+`
