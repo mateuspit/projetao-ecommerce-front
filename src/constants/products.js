d4fe940cc0adfed2be7822619569a698862509a9
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-04-28 03:46:06 -0300

    add products list

---
+++ src/constants/products.js
@@ -0,0 +1,240 @@
+[
+	({
+		name: "Fumo Natural Virginia",
+		description:
+			"O Fumo Natural Virginia é produzido a partir de folhas de tabaco selecionadas, proporcionando um sabor suave e rico. Perfeito para enrolar seu próprio cigarro ou usar em cachimbos e narguilés. Sem aditivos ou conservantes.",
+		image: "https://i.imgur.com/H5bNv2Q.jpg",
+		price: 8.99,
+	},
+	{
+		name: "Seda King Size",
+		description:
+			"As sedas King Size são ideais para enrolar cigarros maiores e mais longos. Feitas de papel de arroz natural e goma arábica, queimam de maneira uniforme e proporcionam uma experiência suave de fumar.",
+		image: "https://i.imgur.com/5jZp0zl.jpg",
+		price: 2.49,
+	},
+	{
+		name: "Narguilé Pequeno",
+		description:
+			"O Narguilé Pequeno é perfeito para quem busca uma experiência de fumar relaxante e saborosa, mas não quer ocupar muito espaço. Fácil de montar, limpar e transportar, este narguilé é perfeito para uso doméstico ou em viagens.",
+		image: "https://i.imgur.com/5xNhWqe.jpg",
+		price: 35.99,
+	},
+	{
+		name: "Essência de Narguilé Menta",
+		description:
+			"A Essência de Narguilé Menta proporciona uma fumaça fresca e refrescante, perfeita para uma sessão relaxante de narguilé. Misture com outras essências para criar sabores únicos e personalizados.",
+		image: "https://i.imgur.com/gQ19xNc.jpg",
+		price: 5.99,
+	},
+	{
+		name: "Incenso de Lavanda",
+		description:
+			"O Incenso de Lavanda oferece um aroma calmante e relaxante, perfeito para aliviar o estresse e criar uma atmosfera tranquila em sua casa. Cada pacote contém 20 varetas de incenso de alta qualidade.",
+		image: "https://i.imgur.com/oxzEpkY.jpg",
+		price: 3.49,
+	},
+	{
+		name: "Fumo de Menta",
+		description:
+			"O Fumo de Menta é uma mistura de tabaco e menta de alta qualidade, proporcionando um sabor refrescante e suave. Perfeito para enrolar cigarros, usar em cachimbos ou misturar com outras variedades de tabaco.",
+		image: "https://i.imgur.com/6ryeAXI.jpg",
+		price: 9.99,
+	},
+	{
+		name: "Seda para Tabaco Slim",
+		description:
+			"As Sedas para Tabaco Slim são ultrafinas e feitas de papel de arroz natural, proporcionando uma queima lenta e uniforme. A embalagem contém 32 folhas, cada uma medindo 110mm x 44mm, perfeitas para enrolar cigarros mais finos.",
+		image: "https://i.imgur.com/qi1DPF6.jpg",
+		price: 1.99,
+	},
+	{
+		name: "Carvão para Narguilé",
+		description:
+			"O Carvão para Narguilé é feito de carvão vegetal de alta qualidade, proporcionando uma queima consistente e duradoura. Perfeito para uso em narguilés e cachimbos de água. Cada pacote contém 10 peças de carvão.",
+		image: "https://i.imgur.com/8GxWrcn.jpg",
+		price: 3.99,
+	},
+	{
+		name: "Filtro para Cigarro",
+		description:
+			"Os Filtros para Cigarro ajudam a reduzir a quantidade de alcatrão e nicotina inalada, proporcionando uma experiência de fumar mais suave. Cada pacote contém 100 filtros descartáveis de alta qualidade.",
+		image: "https://i.imgur.com/dtPQgD8.jpg",
+		price: 2.29,
+	},
+	{
+		name: "Incenso de Sândalo",
+		description:
+			"O Incenso de Sândalo tem um aroma exótico e sensual, perfeito para criar uma atmosfera relaxante e envolvente. Cada pacote contém 20 varetas de incenso de alta qualidade.",
+		image: "https://i.imgur.com/evZmEkd.jpg",
+		price: 3.49,
+	},
+	{
+		name: "Fumo para Cachimbo de Chocolate",
+		description:
+			"O Fumo para Cachimbo de Chocolate oferece um sabor doce e agradável, perfeito para os amantes do chocolate que desejam uma experiência de fumar suave e aromática. Feito a partir de tabaco de alta qualidade e aroma natural de chocolate.",
+		image: "https://i.imgur.com/4xhQ7ja.jpg",
+		price: 10.49,
+	},
+	{
+		name: "Seda para Tabaco Aromatizada de Morango",
+		description:
+			"As Sedas para Tabaco Aromatizadas de Morango adicionam um toque de sabor e aroma às suas sessões de fumar. Feitas de papel de arroz natural e goma arábica, proporcionam uma queima uniforme e suave. Cada pacote contém 32 folhas.",
+		image: "https://i.imgur.com/5pFLLa3.jpg",
+		price: 2.99,
+	},
+	{
+		name: "Cachimbo de Vidro",
+		description:
+			"O Cachimbo de Vidro é feito de vidro borossilicato resistente ao calor e oferece uma maneira elegante e durável de desfrutar do seu tabaco favorito. Seu design permite uma limpeza fácil e manuseio confortável.",
+		image: "https://i.imgur.com/oeAfTlN.jpg",
+		price: 18.99,
+	},
+	{
+		name: "Fumo de Baunilha",
+		description:
+			"O Fumo de Baunilha é uma mistura deliciosa de tabaco e aroma natural de baunilha, oferecendo um sabor doce e suave. Perfeito para enrolar cigarros, usar em cachimbos ou misturar com outras variedades de tabaco.",
+		image: "https://i.imgur.com/FFQhF5W.jpg",
+		price: 9.99,
+	},
+	{
+		name: "Porta Incenso de Cerâmica",
+		description:
+			"O Porta Incenso de Cerâmica é um acessório elegante e funcional para a sua casa. Com um design simples e minimalista, ele se encaixa perfeitamente em qualquer ambiente, mantendo suas varetas de incenso no lugar enquanto queimam.",
+		image: "https://i.imgur.com/ZR6h0vP.jpg",
+		price: 7.49,
+	},
+	{
+		name: "Isqueiro Eletrônico Recarregável",
+		description:
+			"O Isqueiro Eletrônico Recarregável é uma opção ecológica e conveniente para acender cigarros, cachimbos e narguilés. Basta carregá-lo via USB e você terá um isqueiro confiável e seguro, sem necessidade de gás ou chama.",
+		image: "https://i.imgur.com/9aM6PvH.jpg",
+		price: 12.99,
+	},
+	{
+		name: "Fumo para Cachimbo Aromatizado de Cereja",
+		description:
+			"O Fumo para Cachimbo Aromatizado de Cereja oferece um sabor doce e frutado, proporcionando uma experiência de fumar agradável e aromática. Feito a partir de tabaco de alta qualidade e aroma natural de cereja.",
+		image: "https://i.imgur.com/6OzW8Vf.jpg",
+		price: 10.99,
+	},
+	{
+		name: "Cachimbo de Madeira",
+		description:
+			"O Cachimbo de Madeira é uma opção clássica e elegante para apreciar seu tabaco favorito. Feito de madeira de alta qualidade, este cachimbo é durável e oferece uma experiência de fumar suave e saborosa.",
+		image: "https://i.imgur.com/7iE9I9H.jpg",
+		price: 22.99,
+	},
+	{
+		name: "Cinzeiro de Vidro",
+		description:
+			"O Cinzeiro de Vidro é um acessório prático e elegante para a sua casa. Com um design clássico e minimalista, ele se encaixa perfeitamente em qualquer ambiente e é fácil de limpar.",
+		image: "https://i.imgur.com/xqO9Q0v.jpg",
+		price: 9.49,
+	},
+	{
+		name: "Essência de Narguilé Melancia",
+		description:
+			"A Essência de Narguilé Melancia oferece um sabor doce e refrescante, perfeito para uma sessão relaxante de narguilé. Misture com outras essências para criar sabores únicos e personalizados.",
+		image: "https://i.imgur.com/vDd3bxB.jpg",
+		price: 5.99,
+	},
+	{
+		name: "Fumo para Cachimbo de Maçã",
+		description:
+			"O Fumo para Cachimbo de Maçã oferece um sabor doce e frutado, proporcionando uma experiência de fumar agradável e aromática. Feito a partir de tabaco de alta qualidade e aroma natural de maçã.",
+		image: "https://i.imgur.com/Z6y3q3o.jpg",
+		price: 10.49,
+	},
+	{
+		name: "Seda para Tabaco Aromatizada de Uva",
+		description:
+			"As Sedas para Tabaco Aromatizadas de Uva adicionam um toque de sabor e aroma às suas sessões de fumar. Feitas de papel de arroz natural e goma arábica, proporcionam uma queima uniforme e suave. Cada pacote contém 32 folhas.",
+		image: "https://i.imgur.com/6y3jKU1.jpg",
+		price: 2.99,
+	},
+	{
+		name: "Filtro de Vidro para Cigarro",
+		description:
+			"O Filtro de Vidro para Cigarro é uma opção reutilizável e ecológica para reduzir a quantidade de alcatrão e nicotina inalada. Feito de vidro resistente ao calor, é fácil de limpar e proporciona uma experiência de fumar mais suave.",
+		image: "https://i.imgur.com/2Qcs80o.jpg",
+		price: 5.49,
+	},
+	{
+		name: "Incenso de Jasmim",
+		description:
+			"O Incenso de Jasmim oferece um aroma floral e delicado, perfeito para criar uma atmosfera relaxante e romântica em sua casa. Cada pacote contém 20 varetas de incenso de alta qualidade.",
+		image: "https://i.imgur.com/ajCCy3q.jpg",
+		price: 3.49,
+	},
+	{
+		name: "Cachimbo de Metal",
+		description:
+			"O Cachimbo de Metal é uma opção durável e fácil de limpar para apreciar seu tabaco favorito. Com um design moderno e minimalista, este cachimbo é perfeito para uso diário ou ocasional.",
+		image: "https://i.imgur.com/PhUEorW.jpg",
+		price: 14.99,
+	},
+	{
+		name: "Fumo para Narguilé de Canela",
+		description:
+			"O Fumo para Narguilé de Canela oferece um sabor quente e picante, perfeito para uma sessão relaxante e aromática de narguilé. Feito a partir de tabaco de alta qualidade e aroma natural de canela.",
+		image: "https://i.imgur.com/tWn7YcY.jpg",
+		price: 7.99,
+	},
+	{
+		name: "Isqueiro à Prova de Vento",
+		description:
+			"O Isqueiro à Prova de Vento é perfeito para acender cigarros, cachimbos e narguilés em condições de vento ou ao ar livre. Com um design compacto e resistente, ele se encaixa facilmente no bolso ou bolsa.",
+		image: "https://i.imgur.com/6U5d6q5.jpg",
+		price: 8.99,
+	},
+	{
+		name: "Fumo para Narguilé de Limão",
+		description:
+			"O Fumo para Narguilé de Limão oferece um sabor cítrico e refrescante, perfeito para uma sessão relaxante e aromática de narguilé. Feito a partir de tabaco de alta qualidade e aroma natural de limão.",
+		image: "https://i.imgur.com/ZlRyj2O.jpg",
+		price: 7.99,
+	},
+	{
+		name: "Incenso de Lavanda",
+		description:
+			"O Incenso de Lavanda oferece um aroma calmante e relaxante, perfeito para aliviar o estresse e promover um ambiente tranquilo em sua casa. Cada pacote contém 20 varetas de incenso de alta qualidade.",
+		image: "https://i.imgur.com/4zLH4y4.jpg",
+		price: 3.49,
+	},
+	{
+		name: "Seda para Tabaco Aromatizada de Menta",
+		description:
+			"As Sedas para Tabaco Aromatizadas de Menta adicionam um toque de sabor e aroma refrescante às suas sessões de fumar. Feitas de papel de arroz natural e goma arábica, proporcionam uma queima uniforme e suave. Cada pacote contém 32 folhas.",
+		image: "https://i.imgur.com/8Wm7I0v.jpg",
+		price: 2.99,
+	},
+	{
+		name: "Fumo para Narguilé de Manga",
+		description:
+			"O Fumo para Narguilé de Manga oferece um sabor tropical e exótico, perfeito para uma sessão relaxante e aromática de narguilé. Feito a partir de tabaco de alta qualidade e aroma natural de manga.",
+		image: "https://i.imgur.com/oQ2eX8J.jpg",
+		price: 7.99,
+	},
+	{
+		name: "Incenso de Rosas",
+		description:
+			"O Incenso de Rosas oferece um aroma floral e romântico, perfeito para criar uma atmosfera relaxante e envolvente em sua casa. Cada pacote contém 20 varetas de incenso de alta qualidade.",
+		image: "https://i.imgur.com/CVJb0Nu.jpg",
+		price: 3.49,
+	},
+	{
+		name: "Cortador de Charuto",
+		description:
+			"O Cortador de Charuto é uma ferramenta essencial para os apreciadores de charutos. Com lâminas afiadas e precisas, ele permite um corte limpo e suave, preservando o sabor e a integridade do charuto.",
+		image: "https://i.imgur.com/0Bx7AiB.jpg",
+		price: 14.99,
+	},
+	{
+		name: "Fumo para Narguilé de Pêssego",
+		description:
+			"O Fumo para Narguilé de Pêssego oferece um sabor doce e frutado, perfeito para uma sessão relaxante e aromática de narguilé. Feito a partir de tabaco de alta qualidade e aroma natural de pêssego.",
+		image: "https://i.imgur.com/PYEvYnT.jpg",
+		price: 7.99,
+	}),
+];
