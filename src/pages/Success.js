d1e9c886cf5dbb5a16db6620d976b98b50491fa6
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-04-27 22:22:48 -0300

    feat: success static page

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,13 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
 export default function Success() {
-    return(
-        <SuccessContainer>
-            <h1>Compra concluída<br/>com sucesso</h1>
-            <div>
-                <p>Produto 1</p>
-                <p>qntd</p>
-            </div>
-            <p>Data da compra</p>
-            <button>Voltar para a Home</button>
-        </SuccessContainer>
-    )
+	return (
+		<SuccessContainer>
+			<h1>
+				Compra concluída
+				<br />
+				com sucesso
+			</h1>
+			<div>
+				<p>Produto 1</p>
+				<p>qntd</p>
+			</div>
+			<p>Data da compra</p>
+			<button>Voltar para a Home</button>
+		</SuccessContainer>
+	);
 }
+
+const SuccessContainer = styled.div`
+	display: flex;
+    flex-direction: column;
+`;
