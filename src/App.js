35fa4edf129508676e9f836556d156eedfec7645
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-04-27 22:20:41 -0300

    feat: cart static page

--- src/App.js
+++ src/App.js
@@ -2,34 +2,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import styled from "styled-components";
 
-import { UserContext } from "./context/UserContext"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import SignUp from "./pages/SignUp"
-import Product from "./pages/Product"
-import Cart from "./pages/Cart"
-import Checkout from "./pages/Checkout"
-import Success from "./pages/Success"
+import { UserContext } from "./contexts/UserContext";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Success from "./pages/Success";
 
 export default function App() {
-
-  return (
-    <PagesContainer>
-      <BrowserRouter>
-        <UserContext.Provider >
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/product/:id" element={<Product />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/success" element={<Success />} />
-          </Routes>
-        </UserContext.Provider >
-      </BrowserRouter >
-    </PagesContainer >
-  )
+	return (
+		<PagesContainer>
+			<BrowserRouter>
+				<UserContext.Provider>
+					<Routes>
+						<Route path="/login" element={<Login />} />
+						<Route path="/signup" element={<SignUp />} />
+						<Route path="/" element={<Home />} />
+						<Route path="/product/:id" element={<Product />} />
+						<Route path="/cart" element={<Cart />} />
+						<Route path="/checkout" element={<Checkout />} />
+						<Route path="/success" element={<Success />} />
+					</Routes>
+				</UserContext.Provider>
+			</BrowserRouter>
+		</PagesContainer>
+	);
 }
 
 const PagesContainer = styled.main`
